ec7d095c70dca313d2cd11a5a9f20d8b7f21d09b
weiaoxin <weiaoxin@163.com>
2024-07-06 15:57:01 +0800

    add cmp-commandline config

--- plugins/cmp.lua
+++ plugins/cmp.lua
@@ -8,5 +8,24 @@ return{
       opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "copilot", group_index = 2 } }))
     end,
   },
+  {
+    "hrsh7th/cmp-cmdline",
+    config = function()
+      local cmp = require('cmp')
+      cmp.setup.cmdline(':', {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = 'path' }
+      }, {
+        {
+          name = 'cmdline',
+          option = {
+            ignore_cmds = { 'Man', '!' }
+          }
+        }
+      })
+    })
+    end,
+  }
 }
 
