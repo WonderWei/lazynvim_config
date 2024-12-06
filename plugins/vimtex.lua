a4aa36aa7fab75c168f7b9048f0ee3f971707804
weiaoxin <weiaoxin@163.com>
2024-12-06 08:41:46 +0000

    fix Copilot error

--- plugins/vimtex.lua
+++ plugins/vimtex.lua
@@ -1,12 +1,12 @@
--- return {
---   "lervag/vimtex",
---   config = function()
---     vim.cmd("call vimtex#init()")
---     vim.g.vimtex_view_method = "skim"
---     vim.g.vimtex_view_skim_sync = 1
---     vim.g.vimtex_view_skim_activate = 1
---     vim.g.vimtex_quickfix_mode= 0
---   end,
---   -- ft = { 'latex', 'tex' },
---   lazy = false,
--- }
+return {
+  -- "lervag/vimtex",
+  -- config = function()
+  --   vim.cmd("call vimtex#init()")
+  --   vim.g.vimtex_view_method = "skim"
+  --   vim.g.vimtex_view_skim_sync = 1
+  --   vim.g.vimtex_view_skim_activate = 1
+  --   vim.g.vimtex_quickfix_mode= 0
+  -- end,
+  -- -- ft = { 'latex', 'tex' },
+  -- lazy = false,
+}
